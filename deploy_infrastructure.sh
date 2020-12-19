b7807e69d60eb9e4b7bf74d482f131ffaa6acbee
archi144 <mechetin.artur2013@gmail.com>
2020-12-19 11:37:51 +0300

    added scripts

---
+++ deploy_infrastructure.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+config_repo_name = "mephi_ds_bda_hw3_infrastructure"      # name of the github repository with configs and scrips
+app_repo_name    = "mephi_ds_bda_hw3_app"                 # name of the to the github repository with app code
+
+logstash_setup() {
+    logstash_version=$1
+    ls_config_name=$(ls $config_repo_name/configs/logstash/)
+    echo "Downloading Logstash $logstash_version"
+    wget https://artifacts.elastic.co/downloads/logstash/logstash-${logstash_version}-linux-x86_64.tar.gz
+    tar -xf logstash-*tar.gz
+    echo "Setup LS_HOME env"
+    echo "export LS_HOME=$(pwd)/logstash-$logstash_version" >> ~/.bashrc
+    source ~/.bashrc
+    echo "LS_HOME=$LS_HOME is set up "
+    echo "Starting Logstash in the background"
+    nohup $LS_HOME/bin/logstash -f $config_repo_name/configs/logstash/$ls_config_name > logstash.log &
+    echo "Logstash started and listen on 11111 port"
+}
+
+elastic_setup() {
+    elastic_version=$1
+    echo "Downloading Elasticsearch $elastic_version"
+    wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${elastic_version}-linux-x86_64.tar.gz
+    tar -xf elasticsearch-*tar.gz
+    echo "Setup ES_HOME env"
+    echo "export ES_HOME=$(pwd)/elasticsearch-$elastic_version" >> ~/.bashrc
+    source ~/.bashrc
+    echo "ES_HOME=$ES_HOME is set up"
+    echo "Starting Elasticsearch in the background"
+    nohup $ES_HOME/bin/elasticsearch > elasticsearch.log &
+    echo "Elasticsearch started on 9200 port"
+}
+
+grafana_setup() {
+    grafana_version=$1
+    echo "Downloading Grafana $grafana_version"
+    wget https://dl.grafana.com/oss/release/grafana-${grafana_version}.linux-amd64.tar.gz
+    tar -xf grafana-*.tar.gz
+    echo "Setup GRAFANA_HOME env"
+    echo "export GRAFANA_HOME=$(pwd)/grafana-$grafana_version" >> ~/.bashrc
+    source ~/.bashrc
+    echo "GRAFANA_HOME=$GRAFANA_HOME is set up"
+    echo "Provision datasources"
+    cp $config_repo_name/configs/grafana/provisioning/datasources/datasource.yaml $GRAFANA_HOME/conf/provisioning/datasources/
+    echo "Provision dashboards"
+    cp -r $config_repo_name/configs/grafana/provisioning/dashboards/* $GRAFANA_HOME/conf/provisioning/dashboards/
+    nohup $GRAFANA_HOME/bin/grafana-server -homepath $GRAFANA_HOME web > grafana.log &
+    echo "Grafana started on 3000 port"
+}
+
+app_setup() {
+    pull_app
+    sudo apt install -y python3
+    sudo apt install -y python3-pip
+    cd $app_repo_name
+    pip3 install -r requirements.txt
+    python3 main.py pizza
+}
+
+main () {
+    sudo apt update -y
+    elastic_setup 7.10.1
+    logstash_setup 7.10.1
+    app_setup
+    grafana_setup 7.3.5
+}
+
+main
---
+++ prepare_environment.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+config_repo_name = "mephi_ds_bda_hw3_infrastructure"      # name of the github repository with configs and scrips
+app_repo_name    = "mephi_ds_bda_hw3_app"                 # name of the to the github repository with app code
+
+virtualbox_setup() {
+    sudo yum install –y patch gcc kernel-headers kernel-devel make perl wget
+    sudo wget http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -P /etc/yum.repos.d
+    sudo yum install -y VirtualBox-6.0
+}
+
+vagrant_setup() {
+    vagrant_version=$1
+    echo "Installing Vagrant $vagrant_version..."
+    sudo yum install -y https://releases.hashicorp.com/vagrant/${vagrant_version}/vagrant_${vagrant_version}_x86_64.rpm && vagrant --version
+    echo "Making vagrant dir"
+    mkdir -p ~/vagrant-ubuntu && cd ~/vagrant-ubuntu
+    echo "Pulling configs and scripts from github"
+    pull_configs
+    echo "Pull configs and scripts succeed"
+    cp $config_repo_name/configs/vagrant/Vagranfile .
+    sudo sysctl -w vm.max_map_count=262144                # it's need to just for avoid the posiible future problems
+    echo "Pulling app"
+    pull_app
+    echo "Pull app succeed"
+    echo "Substitute secret files..."
+    cp -u ~/CREDENTIALS.txt $app_repo_name/
+    echo "Substitution succeed"
+    echo "Starting Vagrant VM"
+    vagrant up
+    echo "Uploading configs and scripts to the guest"
+    vagrant upload $config_repo_name
+    echo "Upload succeed"
+    echo "Uploading app sources to the guest"
+    vagrant upload $app_repo_name
+    echo "Upload succeed"
+}
+
+pull_configs() {
+    git clone https://github.com/archi144/$config_repo_name.git
+}
+
+pull_app() {
+    git clone https://github.com/archi144/$app_repo_name.git
+}
+
+main() {
+    sudo yum update -y
+    virtualbox_setup
+    vagrant_setup 2.2.6
+}
+
+main
